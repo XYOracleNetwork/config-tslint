38987e6f0d8d21000eae12a02b50303dfded395b
Arie Trouw <arie.trouw@xyfindables.com>
2018-11-13 12:49:06 -0800

    Initial commit

---
+++ tslint.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  rulesDirectory: [
+    path.join(path.dirname(require.resolve('tslint-consistent-codestyle')), './'),
+    path.join(path.dirname(require.resolve('tslint-eslint-rules')), 'dist/rules'),
+    path.join(path.dirname(require.resolve('tslint-microsoft-contrib')), './'),
+  ],
+  rules: {
+    "no-inferrable-types": [true, "ignore-params"],
+    "trailing-comma": false,
+    "no-increment-decrement": false,
+    "semicolon": [true, "never"],
+    "ordered-imports": false,
+    "import-name": false,
+    "object-shorthand-properties-first": false,
+    "align": false,
+    "max-line-length": [true, {"limit": 120, "ignore-pattern": "^import |^export {(.*?)}"}],
+    "object-literal-sort-keys": false,
+    "no-implicit-dependencies": [true, "dev"],
+    "interface-name":false,
+    "no-bitwise": false,
+    "quotemark": [true, "single", "avoid-escape", "avoid-template"],
+    "no-console": false,
+    "function-name": false,
+  },
+};
