cb33a6f514f0140cb7d68bb6fcbc14004307b11f
Arie Trouw <arie.trouw@xy.company>
2019-04-18 11:30:11 -0700

    Updates

--- tslint.js
+++ tslint.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "$schema": "http://json-schema.org/draft-04/schema#",
+  "$schema": "http://json.schemastore.org/tslint",
   "extends": ["tslint-config-airbnb", "tslint-react"],
   "rules": {
     "no-inferrable-types": [true, "ignore-params"],
@@ -7,16 +7,17 @@ module.exports = {
     "no-increment-decrement": false,
     "semicolon": [true, "never"],
     "ordered-imports": false,
-    "import-name": false,
-    "object-shorthand-properties-first": false,
-    "align": false,
+    "object-shorthand-properties-first": true,
     "max-line-length": [true, {"limit": 180, "ignore-pattern": "^import |^export {(.*?)}"}],
     "object-literal-sort-keys": false,
-    "no-implicit-dependencies": [true, "dev"],
+    "no-implicit-dependencies": [true, "dev", "optional"],
     "interface-name":false,
     "no-bitwise": false,
     "no-console": false,
     "function-name": false,
     "quotemark": [true, "single", "backtick", "avoid-escape", "avoid-template"],
+    "no-implicit-dependencies": [true, "dev", "optional"],
+    "interface-name": [true, "always-prefix"],
+    "align": [true, "parameters", "arguments", "members", "elements", "statements"]
   },
 };
